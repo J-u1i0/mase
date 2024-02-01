bce68f60c7d36157b864e18c3b209ae413d3d460
J-u1i0 <Julio.Castillejo.Motta@gmail.com>
2024-02-01 16:13:31 +0000

    Unpacked tuple bug in transform.py

--- machop/chop/actions/transform.py
+++ machop/chop/actions/transform.py
@@ -53,7 +53,7 @@ def transform(
     # graph generation
     graph = MaseGraph(model=model, cf_args=cf_args)
     # graph_metadata = Mase
-    graph = init_metadata_analysis_pass(graph, pass_args=None)
+    graph, _ = init_metadata_analysis_pass(graph, pass_args=None)
     # logger.info(f"graph: {graph.fx_graph}")
 
     # create or load metadata.parameters and mase_graph.model
